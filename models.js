588522cca4ae1a86259b3df8ceaa40ce4818dbe5
zahra dehghanian <z.dehghanian@aut.ac.ir>
2018-12-12 13:44:36 +0330

    here we are

--- models.js
+++ models.js
@@ -1,139 +1,24 @@
 let mongodb = require('mongodb')
-//using mongodb for data base 
-//let nodemailer = require('nodemailer');
-module.exports.CreateCollection= function (collectionName)
-{
-    mongodb.connect('mongodb://localhost:27017/',function(err, db){
-    if (err) throw err;
-    console.log("you are now connected to your database");
-    var dbo = db.db("hospital");
-    dbo.createCollection(collectionName , function(err , res ){
-    if (err) throw err;
-    console.log('creat collection');
-    dbo.collection(collectionName).insertOne({universityCode : 'zahrajan' , password: "1376" , unicode:"610395136"},function(err,res){
-        if(err) throw err ;
-        console.log('zahrajan is added pass : 1376');
-    })
-    db.close();
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let userSchema = new Schema({
+    universityCode : String,
+    email : String,
+    password : String,
+    confirmPassword : String,
+    authenticateId : String
 });
-});
-}
-//creating a collection in db
-
-module.exports.login= function (collectionName , user, response , page)
-{   let returnable= {pageName : '',
-                user : {}
-            };
-    console.log("slm asisam");
-    mongodb.connect('mongodb://localhost:27017/',function(err, db){
-        if (err) throw err;
-        var dbo = db.db("hospital");
-            dbo.collection(collectionName).findOne({universityCode: user.universityCode}, function(err ,result){
-                //if exist
-                
-                if(result == null){
-                    console.log(user.universityCode + " was not found!");
-                    page.last = "/login"
-                    returnable.pageName = 'login';
-                   
-                }
-                //if not 
-                else{       
-                    if(user.universityCode == result.universityCode && user.password == result.password){
-                        //dbo.collection("user").insertOne(user,function(err , res){
-                            console.log("user has been added!")
-                            if (err) throw err;
-                            page.last = "/profile"
-                            returnable.pageName = 'profile';
-                            returnable.user = user;
-                            
-                        //});
-                    }
-                    else{    
-                        page.last = "/login";
-                        console.log(page);
-                        returnable.pageName = 'login';
-                        
-                    }
-                }    
-               
-            
-            });
-        db.close();
-    });
-    return(returnable);
-}                  
-
-//find some one in db 
-// if it was in it so render profile
-//else it was render login
-
-module.exports.DeleteMany = function (collectionName)
-{
-    mongodb.connect('mongodb://localhost:27017/',function(err, db){
-    if (err) throw err;
-    console.log("you are now connected to your database");
-    var dbo = db.db("hospital");
-    dbo.collection(collectionName).deleteMany( {}, function(err , res ){
-    if (err) throw err;
-    console.log("all users are deleted !");
-})
-    db.close();
-});
-}
-//delete all users in db
-
-module.exports.InsertOne = function (collectionName,user)
-{
-    mongodb.connect('mongodb://localhost:27017/',function(err, db){
-    if (err) throw err;
-    console.log("you are now connected to your database");
-    var dbo = db.db("hospital");
-    dbo.collection(collectionName).insertOne( user , function(err , res ){
-    if (err) throw err;
-    console.log(user.universityCode + " is added !");
-});
-    db.close();
-});
-}
-//add a user to db
-module.exports.UpdateOne = function (collectionName,pastQuery,newQuery)
-{
-    mongodb.connect('mongodb://localhost:27017/',function(err, db){
-    if (err) throw err;
-    console.log("you are now connected to your database");
-    var dbo = db.db("hospital");
-    dbo.collection(collectionName).updateOne(pastQuery , {$set : newQuery}, function(err , res ){
-    if (err) throw err;
-    console.log(collectionName + " is updated !");
-});
-    db.close();
-});
-}
-
-
+module.exports.user = mongoose.model('user', userSchema);
+let nodemailer = require('nodemailer');
 ///
-/*
-module.exports.C reateTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
+
+module.exports.CreateTransport = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth:
+  {
+      user: 'nizomaan@gmail.com',
+      pass: 'nizomanadmin'
   }
 });
-
-var mailOptions = {
-  from: 'youremail@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-*/
--- server.js
+++ server.js
@@ -1,15 +1,57 @@
 let express = require('express');
-let mongodb = require('mongodb').MongoClient;
+//let mongodb = require('mongodb');
+//let mongoclient = require('mongodb').MongoClient;
 //let nodemailer = require('nodemailer');
-let CreateCollection = require('./models').CreateCollection;
+//let CreateCollection = require('./models').CreateCollection;
 //for creating collection
-let login = require ('./models').login;
+//let FindOne = require ('./models').FindOne;
 // for finding some one in sign in (collectionName, user , response , page)
-let InsertOne = require('./models').InsertOne;
+//let InsertOne = require('./models').InsertOne;
 //for inserting a user (collectionName , user)
-let DeleteMany = require('./models').DeleteMany;
+//let DeleteMany = require('./models').DeleteMany;
 //for deleting all users (collectoinName)
-page = {login : "/login",profile : "/profile", signup : "/signup", last : "/login" }
+let mongoose = require('mongoose');
+
+let id ;
+
+mongoose.connect('mongodb://Devaince:Loganlogan1@ds237815.mlab.com:37815/hospital');
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+let user = require('./models').user;
+
+let nodemailer = require('nodemailer');
+let mailOptions = {
+    from: 'youremail@gmail.com',
+    to: 'myfriend@yahoo.com',
+    subject: 'Sending Email using Node.js',
+    text: '123456:))'
+  };
+let transporter = require('./models').CreateTransport;
+
+// let samin =  user({
+//     universityCode : "sam",
+//     password : "ans",
+//     email : "alireza.kamgar@gmail.com",
+//     confirmPassword : "56256"
+// });
+
+//samin.save(function(err)
+// {
+//     if (err) throw ("hgtfjytdtrshreayrharrhererehrestheshtrete");
+//     console.log("salam samin joooooon!");
+// });
+
+
+console.log("khobi?");
+
+
+page = {login : "/login",profile : "/Users/profile", signup : "/signup", last : "/login" }
 // web pages
  
 let bodyParser = require('body-parser')
@@ -17,91 +59,142 @@ let urlencodedParser = bodyParser.urlencoded({ extended: false })
 //body parser
 
 //let transpoter
-var x;
-mongodb.connect('mongodb://localhost:27017/',function(err, db){
-        if (err) throw err;
-        console.log(db);
-    });
-    console.log(x);
-    var dbo = x.db("hospital");
-    dbo.createCollection(collectionName , function(err , res ){
-    if (err) throw err;
-    console.log('creat collection');
-    dbo.collection(collectionName).insertOne({universityCode : 'zahrajan' , password: "1376" , unicode:"610395136"},function(err,res){
-        if(err) throw err ;
-        console.log('zahrajan is added pass : 1376');
-    });
-});
-  
+
+
+
+
 let app = express();
 //app is made
 
+//CreateCollection("users",ali);
 //###
-//CreateCollection("users");
-//###
-
-
 
 app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine' , 'ejs');
 
-app.get('/login' , function(req , res){
+app.get('/login',function(req , res ,next ){
     console.log('login o run kardam');
     res.render('login',page);
-});
-app.use('/login',urlencodedParser,function (req,res){
-    console.log("page : " + page.profile);
-    // enter to profile
-    let user = { 
+    });
+
+app.use('/login',urlencodedParser,function(req , res, next){
+    let loginUser = { 
         universityCode : req.body.universityCode,
         password : req.body.password
-        
     };
-    console.log(user);
-    let returnable = login("users" , user , res , page);
-    console.log(returnable.pageName +"this is page name");
-    returnable.pageName == 'login'? res.redirect('login') : res.render(returnable.pageName,page);
+    console.log(loginUser);
+    user.find({universityCode : loginUser.universityCode ,password : loginUser.password }).exec(function(err ,docs){
+        if(docs == "")
+        {
+            console.log("it is not found");
+            res.redirect('login');
+        }        
+        else
+        {
+            id = docs[0]._id;
+            docs[0].authenticateId = id;
+            docs[0].save(function(err)
+            {
+                if (err) throw err;
+            });
+
+            console.log(docs[0].authenticateId );
 
 
 
-});
-app.post('/login',function(req , res ,next ){
-    //console.log('/login o run kardam' + req.url);
-        next();
-        res.redirect("login");
+            res.writeHead(302, {
+                'Location': '/Users/profile'
+                //add other headers here...
+              });
+            //res.render('profile',docs[0])
+            res.end();
+            
+            console.log("it is found");
+        }
     });
-// app.get('/person/:id' , function(req , res){
-//     res.render('person' ,{aydi : req.params.id , Qiustr: req.query.qstr });
-// }); 
 
-app.post(page.profile ,urlencodedParser, function(req , response){
-    
+}); 
 
-});
-
-app.use(page.signup , function(req , response, next){
+app.get('/signup', function(req , response){
+    console.log("signup")
     response.render('signup',page);
-    next();
+    
 });
-app.use(page.login , urlencodedParser , function(req , response){
-    let user = { 
+
+app.use('/signup' , urlencodedParser , function(req , response){
+    console.log("sign up")
+    let newUser = user({ 
     universityCode : req.body.universityCode,
-    phoneNumber : req.body.phoneNumber,
+    email : req.body.email,
     password : req.body.password,
     confirmPassword : req.body.confirmPassword
-    };
-    if(user.password == user.confirmPassword)
+    });
+    console.log(newUser);
+    if(newUser.password == newUser.confirmPassword)
     {
-        InsertOne("users" , user);
-        console.log("insert kardam");
-        response.render('login',page);
+        console.log("asdasds");
+        newUser.save(function(err)
+        {
+            if (err) throw err;
+        });
+        transporter.sendMail(mailOptions, function(error, info)
+        {
+            if (error)
+            {
+                console.log(error);
+            }
+            else 
+            {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        console.log("u are added");
+        
+        
+        response.writeHead(302, {
+        'Location': '/login'
+        //add other headers here...
+        });
+        response.end();
     }
     else
     {
-        response.render('signup',page);
-        
+        console.log("sign up dobare");
+        response.redirect('signup');
+        //pass != confirm    
     }    
 });
+
+
+
+console.log("profile debug");
+app.use('/Users/profile' ,function(req , response){
+    console.log("page : " + page.profile);
+    // enter to profile
+    console.log(id, ": haza id");
+    user.find({_id : id}).exec(function(err, docs){
+        if(docs[0].authenticateId == docs[0]._id)
+        {
+            console.log('profile dg');
+            response.render('profile',docs[0]);
+        }
+        else
+        {
+            response.writeHead(302, {
+                'Location': '/login'
+                //add other headers here...
+                });
+                response.end();
+        }
+    });
+    });            
+    
+
+
+
+
+
+
 //app.use(page.mail , function(req , response , next){
 //});
 
@@ -117,7 +210,7 @@ app.use(page.login , urlencodedParser , function(req , response){
 // });
 
 // app.use(function(req, res , next){
-//     let err = new Error("404 not found");
+//     let err = new Error("404 not found");     
 //     err.status = 404
 // });
 // app.use(function(err, req, res, next){
